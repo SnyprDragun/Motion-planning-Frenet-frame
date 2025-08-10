normalization fixed

scripts/CartesianFrenetConverter.py
@@ -1,19 +1,6 @@
 #!/Users/subhodeep/venv/bin/python
 
-"""
-
-A converter between Cartesian and Frenet coordinate systems
-
-author: Wang Zheng (@Aglargil)
-
-Reference:
-
-- [Optimal Trajectory Generation for Dynamic Street Scenarios in a Frenet Frame]
-(https://www.researchgate.net/profile/Moritz_Werling/publication/224156269_Optimal_Trajectory_Generation_for_Dynamic_Street_Scenarios_in_a_Frenet_Frame/links/54f749df0cf210398e9277af.pdf)
-
-"""
-
-import math
+import numpy as np
 
 class CartesianFrenetConverter:
     """
@@ -46,15 +33,15 @@ class CartesianFrenetConverter:
         dx = x - rx
         dy = y - ry
 
-        cos_theta_r = math.cos(rtheta)
-        sin_theta_r = math.sin(rtheta)
+        cos_theta_r = np.cos(rtheta)
+        sin_theta_r = np.sin(rtheta)
 
         cross_rd_nd = cos_theta_r * dy - sin_theta_r * dx
-        d = math.copysign(math.hypot(dx, dy), cross_rd_nd)
+        d = np.copysign(np.hypot(dx, dy), cross_rd_nd)
 
         delta_theta = theta - rtheta
-        tan_delta_theta = math.tan(delta_theta)
-        cos_delta_theta = math.cos(delta_theta)
+        tan_delta_theta = np.tan(delta_theta)
+        cos_delta_theta = np.cos(delta_theta)
 
         one_minus_kappa_r_d = 1 - rkappa * d
         d_dot = one_minus_kappa_r_d * tan_delta_theta
@@ -102,8 +89,8 @@ class CartesianFrenetConverter:
             raise ValueError(
                 "The reference point s and s_condition[0] don't match")
 
-        cos_theta_r = math.cos(rtheta)
-        sin_theta_r = math.sin(rtheta)
+        cos_theta_r = np.cos(rtheta)
+        sin_theta_r = np.sin(rtheta)
 
         x = rx - sin_theta_r * d_condition[0]
         y = ry + cos_theta_r * d_condition[0]
@@ -111,8 +98,8 @@ class CartesianFrenetConverter:
         one_minus_kappa_r_d = 1 - rkappa * d_condition[0]
 
         tan_delta_theta = d_condition[1] / one_minus_kappa_r_d
-        delta_theta = math.atan2(d_condition[1], one_minus_kappa_r_d)
-        cos_delta_theta = math.cos(delta_theta)
+        delta_theta = np.atan2(d_condition[1], one_minus_kappa_r_d)
+        cos_delta_theta = np.cos(delta_theta)
 
         theta = CartesianFrenetConverter.normalize_angle(delta_theta + rtheta)
 
@@ -123,7 +110,7 @@ class CartesianFrenetConverter:
             cos_delta_theta / one_minus_kappa_r_d
 
         d_dot = d_condition[1] * s_condition[1]
-        v = math.sqrt(one_minus_kappa_r_d * one_minus_kappa_r_d *
+        v = np.sqrt(one_minus_kappa_r_d * one_minus_kappa_r_d *
                       s_condition[1] * s_condition[1] + d_dot * d_dot)
 
         delta_theta_prime = one_minus_kappa_r_d / cos_delta_theta * kappa - rkappa
@@ -139,7 +126,4 @@ class CartesianFrenetConverter:
         """
         Normalize angle to [-pi, pi]
         """
-        a = math.fmod(angle + math.pi, 2.0 * math.pi)
-        if a < 0.0:
-            a += 2.0 * math.pi
-        return a - math.pi
+        return (angle + np.pi) % (2 * np.pi) - np.pi